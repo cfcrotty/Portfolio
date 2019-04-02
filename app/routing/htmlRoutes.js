a56a12bc5b35b10a7b991be7eac20fd492b9f75a
cfcrotty <carafeliselana@gmail.com>
2019-04-01 19:08:33 -0700

    update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,13 +3,10 @@ const path = require("path");
 const fs = require("fs");
 
 module.exports = (app) => {
-    // app.get("/", (req, res) => {
-    //     res.sendFile(path.join(__dirname, "../public/main.html"));
-    // });
     app.get('*', function (req, res) {
         let newArr = req.params[0].split("/");
         let page = newArr[1] || "home";
-        if (page == "home" || page == "contact" || page == "portfolio" && !newArr[2] ) {
+        if ((page == "home" || page == "contact" || page == "portfolio") && !newArr[2] ) {
             fs.readFile(__dirname + `/../public/${page}.html`, function (err, pageRes) {
                 var data = {
                     data: pageRes
@@ -17,10 +14,12 @@ module.exports = (app) => {
                 fs.readFile(__dirname + '/../public/main.html', 'utf-8', function (error, source) {
                     var template = handlebars.compile(source);
                     var html = template(data);
+                    res.status(200);
                     res.send(html);
                 });   
             });
         } else {
+            res.status(404);
             res.sendFile(path.join(__dirname, "../public/error.html"));
         }
     });
